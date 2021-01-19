f0c457e87e82cf33fcc2c4b24d650919c17c6ebf
lteruel10 <lteruel10@gmail.com>
2021-01-18 22:23:57 -0600

    16 Funciones: Parametros opcionales, obligatorios y por defecto

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 "use strict";
 (function () {
-    function getEdad() {
-        return 100 + 100 + 300;
+    //function activar(quien:string,objeto:string  ){//obligatorios
+    //function activar(quien:string,objeto:string='batisenal'  ){//objeto es opcional muy interesante
+    function activar(quien, momento, objeto) {
+        //ORDEN OBLIGATORIO OPCIONALES y por ultimo los que tienen VALORES POR DEFECTO      
+        if (objeto === void 0) { objeto = 'batisenal'; }
+        if (momento) {
+            console.log(quien + " activo la " + objeto + " en la " + momento);
+        }
+        else {
+            console.log(quien + " activo la " + objeto);
+        }
     }
-    var nombre = 'Fernando';
-    var apellido = 'Teruel';
-    var edad = 49;
-    //const salida=nombre+apellido+edad; /// veamos una mejor forma de hacerlo
-    ///const salida=nombre+" " +apellido+" ("+edad+")";//tampoco
-    //mejor utilizr TEMPLATES LITERALES
-    // const salida=`${nombre} \n${apellido} \n${edad}`;// ``se llama back tips o algo asi
-    //ahora mejorada la linea anterior
-    var salida = "\n    " + nombre + "\n    " + apellido + "\n    (" + getEdad() + ")"; //(${ edad+100})`;
-    console.log(salida);
+    activar('Luis', 'TRDE');
 })(); //SCOPE //funcion auto invocado  ///PATRON MODULO
 //CUANDO UTILIZAMOS LET LAS VARIABLES SOLO EXISTEN EN EL SCOPE...despues ya no exiisten
