111193ae5a1750a08fabff2297fa241314a50c77
lteruel10 <lteruel10@gmail.com>
2021-01-18 22:58:22 -0600

    17 Funciones de flecha full

--- typescript/funcionesFlecha.ts
+++ typescript/funcionesFlecha.ts
@@ -13,4 +13,32 @@
     console.log(mifuncion('Normal'));
     console.log(mifuncionF('Flecha con {}'));
     console.log(mifuncionF2('Flecha'));
+
+    const sumarN=function(a:number,b:number){
+        return a+b;
+    }
+    const sumarF=(a:number,b:number)=>(a+b);
+
+    console.log(sumarN(2,2));
+    console.log(sumarF(8,8));
+
+
+    //veamos otra funcion
+    const hulk ={
+        nombre:'Hulk',
+        smash(){
+            setTimeout(() => {
+                console.log(`${this.nombre} smash!!!`);
+            }, 1000);
+
+            setTimeout(() =>{
+                console.log(`${this.nombre} smash!!!`);
+            },2000);///2000 milisegundos
+            //this sirve apra apunta al objeto 
+        }
+    }
+
+    hulk.smash();
+
+
 })();//SCOPE 
