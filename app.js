6d07d9821a3d8f44885ecc82f94f0f683b158d57
lteruel10 <lteruel10@gmail.com>
2021-01-20 09:09:34 -0600

    23 Definicion de una clase basica

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 "use strict";
 (() => {
-    const enviarMision2 = (xmen) => {
-        console.log(`Enviando a ${xmen.nombre} a la mision `);
-    }; //enviar mision
-    const regresarAlCuartel2 = (xmen) => {
-        console.log(`Regresando a ${xmen.nombre} de la mision `);
-    }; //regresa mision
-    const wolverine2 = {
-        nombre: 'Logan',
-        edad: 60
-    };
-    enviarMision2(wolverine2);
-    regresarAlCuartel2(wolverine2);
+    //clases aspercion de algo de la vida real
+    //metodos // son las acciones que uede hacer el objeto
+    //herencia **LA CLASE1 le herede todas sus PROPIEDADES A OTRA CLASE puede ser CLASE 2
+    class Avenger {
+        constructor() {
+            ///propiedades
+            this.nombre = 'Sin Nombre';
+        }
+    }
+    const antman = new Avenger();
+    console.log(antman);
 })(); //SCOPE 
--- app.ts
+++ app.ts
@@ -1,41 +1,19 @@
 (()=>{//SCOPE
-    //interfases de typscript
-    //**********SIN INTERFACE */
-/*    
-const enviarMision=(xmen:{nombre:string})=>{ //const enviarMisidon=(xmen:any)=>{ no es adecuada x any
-    console.log(`Enviando a ${xmen.nombre} a la mision `)
-}//enviar mision
-const regresarAlCuartel=(xmen:{nombre:string})=>{
-    console.log(`Regresando a ${xmen.nombre} de la mision `)
-}//regresa mision
-const wolverine={
-    nombre: 'Logan',
-    edad:60
-}
-enviarMision(wolverine);
-regresarAlCuartel(wolverine); 
-*/
-//si tuvieramos que cambiar el elemento nombre por nombreXMen... habria que cambiarlo en todo el codigo
-//a continuacion una solucion
-//**********CREANDO UNA INTERFACE */ son formas de definir REGLAS
-interface XMen {//reglas que cumple un objeto, al final viene siendo una clase
-    nombre:string;
-    edad:number;
-    poder?:string;//el simbolo ? me dice que es opcional
-}//iface
+//clases aspercion de algo de la vida real
+//metodos // son las acciones que uede hacer el objeto
+//herencia **LA CLASE1 le herede todas sus PROPIEDADES A OTRA CLASE puede ser CLASE 2
 
-const enviarMision2=(xmen:XMen)=>{ 
-    console.log(`Enviando a ${xmen.nombre} a la mision `)
-}//enviar mision
-const regresarAlCuartel2=(xmen:XMen)=>{
-    console.log(`Regresando a ${xmen.nombre} de la mision `)
-}//regresa mision
-const wolverine2:XMen={///al agregar :XMen nos obliga a mantener el tipado
-    nombre: 'Logan',
-    edad:60
-}
-enviarMision2(wolverine2);
-regresarAlCuartel2(wolverine2);
+    class Avenger{//las clases siempre empiezan con letra capital
+        
+        ///propiedades
+        nombre:string='Sin Nombre';
+        equipo:string;
+        nombreReal:string;
+        poderPelear:boolean;
+        peleasGanadas:number;
+    }
+    const antman=new Avenger();
+    console.log(antman);
 
 
 })();//SCOPE 
