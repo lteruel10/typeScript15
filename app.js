2e8cfebd6c7b40793f828ad16cdd0b8101edcd43
lteruel10 <lteruel10@gmail.com>
2021-01-20 08:52:21 -0600

    21 Interfaces de tyoescript

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 "use strict";
 (() => {
-
-
-
-})(); //SCOPE
+    const enviarMision2 = (xmen) => {
+        console.log(`Enviando a ${xmen.nombre} a la mision `);
+    }; //enviar mision
+    const regresarAlCuartel2 = (xmen) => {
+        console.log(`Regresando a ${xmen.nombre} de la mision `);
+    }; //regresa mision
+    const wolverine2 = {
+        nombre: 'Logan',
+        edad: 60
+    };
+    enviarMision2(wolverine2);
+    regresarAlCuartel2(wolverine2);
+})(); //SCOPE 
