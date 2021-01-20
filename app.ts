154ff6f8cf8d8a5356b504dc9c9c4eca5aa7e084
lteruel10 <lteruel10@gmail.com>
2021-01-19 19:39:24 -0600

    19 Promesas

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-(function(){//SCOPE
+(()=>{//SCOPE
   
 
 
---
+++ typescript/promesas1.ts
@@ -0,0 +1,19 @@
+(()=>{//SCOPE
+  
+//PROMESAS INTRODUCCION
+console.log(  'inicio');
+
+const prom1=new Promise((resolve,reject)=>{  //resolve es lo que retorna cuando funciona bien
+                                                //   reject cuando no funciona o da error
+     setTimeout (()=>{
+         reject('Se termino el timeOut');
+     },1000);
+})//prom1                                        
+
+prom1
+    .then(mensaje =>console.log(mensaje) )
+    .catch(err=>console.warn(err));
+console.log('Fin');
+
+
+})();//SCOPE 
