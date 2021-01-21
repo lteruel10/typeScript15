29dcab8564cba3487ee711a49a0a12a4e8b020f4
lteruel10 <lteruel10@gmail.com>
2021-01-20 21:23:55 -0600

    24 constructores de una clase

--- app.js
+++ app.js
@@ -3,12 +3,42 @@
     //clases aspercion de algo de la vida real
     //metodos // son las acciones que uede hacer el objeto
     //herencia **LA CLASE1 le herede todas sus PROPIEDADES A OTRA CLASE puede ser CLASE 2
-    class Avenger {
-        constructor() {
+    /*
+        class Avenger{//las clases siempre empiezan con letra capital
             ///propiedades
-            this.nombre = 'Sin Nombre';
+            nombre:string='Sin Nombre';
+            equipo:string;
+            nombreReal:string;
+            poderPelear:boolean;
+            peleasGanadas?:number;
+            
+            //BOB
+            constructor(nombre:string, equipo:string, nombreReal:string,poderPelear:boolean,
+                peleasGanadas:number){
+                this.nombre=nombre;
+                this.equipo=equipo;
+                this.nombreReal=nombreReal;
+                this.poderPelear=poderPelear;
+                this.peleasGanadas=peleasGanadas;
+            }
+      */
+    class Avenger {
+        ///propiedades
+        /*   nombre:string='Sin Nombre';
+           equipo:string;
+           nombreReal:string;
+           poderPelear:boolean;
+           peleasGanadas?:number;
+         */
+        //BOB
+        constructor(nombre, equipo, nombreReal, poderPelear, peleasGanadas = 0) {
+            this.nombre = nombre;
+            this.equipo = equipo;
+            this.nombreReal = nombreReal;
+            this.poderPelear = poderPelear;
+            this.peleasGanadas = peleasGanadas;
         }
     }
-    const antman = new Avenger();
+    const antman = new Avenger('antman', 'capi');
     console.log(antman);
 })(); //SCOPE 
